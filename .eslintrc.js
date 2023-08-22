d6cc904ff7da5a94ee0a595f387ac45916f604e4
Felipe Scherer <felipescherer25@gmail.com>
2023-08-21 21:09:21 -0300

    :sparkles: feat: lint-staged husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
-module.exports = {
-  extends: ['@antfu'],
+// .eslintrc.js
+const process = require('node:process')
 
+process.env.ESLINT_TSCONFIG = 'tsconfig.json'
+
+module.exports = {
+  // "parser": "@typescript-eslint/parser",
+  extends: ['@antfu', 'next/core-web-vitals'],
 }
