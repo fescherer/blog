c48255ad548488822ea5d5065a40fef3e27d6db6
Felipe Scherer <felipescherer25@gmail.com>
2023-08-28 13:36:08 -0300

    :wrench: chore projects page

--- src/features/Posts/index.tsx
+++ src/features/Posts/index.tsx
@@ -3,10 +3,17 @@ import { allDocs } from 'contentlayer/generated'
 
 export function Posts() {
   return (
-    <div className='flex flex-wrap gap-2'>
+
+  // <div className='grid'>
+    // <div className='flex justify-center'>
+    //         <div className='flex flex-wrap gap-2'>
+    <div className='grid w-full grid-flow-row grid-cols-4 justify-between justify-items-center gap-5'>
       {
         allDocs.map(item => <PostCard key={item._id} data={item} />)
-      }
+        }
     </div>
+    //   </div>
+    // </div>
+
   )
 }
