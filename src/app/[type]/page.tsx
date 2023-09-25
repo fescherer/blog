c736dc20ee2e207b67bc29a9cf3f970aaae94c39
Felipe Scherer <felipescherer25@gmail.com>
2023-09-24 22:18:32 -0300

    :sparkles: feat sitemap

--- src/app/[type]/page.tsx
+++ src/app/[type]/page.tsx
@@ -6,27 +6,26 @@ import { allDocs } from 'contentlayer/generated'
 
 interface PostsByCategoryProps {
   params: {
-    category: string
-
+    type: string
   }
 }
 
-export default function PostsByCategory({ params: { category } }: PostsByCategoryProps) {
-  const docs = allDocs.filter(doc => doc.categories.includes(category))
+export default function PostsByCategory({ params: { type } }: PostsByCategoryProps) {
+  const docs = allDocs.filter(doc => doc.categories.includes(type))
 
   return (
     <>
       <div className='m-auto flex w-full flex-col gap-4'>
-        <h2 className='text-center text-sm capitalize'><Link aria-label="Remove filtered by:" target='_self' href="/">Filtered by:{category}</Link></h2>
+        <h2 className='text-center text-sm capitalize'><Link aria-label="Remove filtered by:" target='_self' href="/">Filtered by:{type}</Link></h2>
         <Posts docs={docs} />
       </div>
       <aside className='hidden flex-col gap-2 lg:flex'>
-        <AsideAllTagsAndCategories selectedCategory={category} />
+        <AsideAllTagsAndCategories selectedCategory={type} />
       </aside>
       <aside className='block lg:hidden'>
         <MobileMenuArticle>
           <div className='flex flex-col gap-2 '>
-            <AsideAllTagsAndCategories selectedCategory={category} />
+            <AsideAllTagsAndCategories selectedCategory={type} />
           </div>
         </MobileMenuArticle>
       </aside>
