e384f84e4a24f83a11caa8b986f90b0d8558bda6
Felipe Scherer <felipescherer25@gmail.com>
2023-11-12 01:09:25 -0300

    :recycle: refactor rss

---
+++ src/utils/generateRSS.ts
@@ -0,0 +1,46 @@
+import fs from 'node:fs'
+import { Feed } from 'feed'
+import { allDocs } from 'contentlayer/generated'
+import { ownerConfigs, ownerMetaData } from '@/utils/ownerConfigs'
+
+export function generateRss() {
+  const feed = new Feed({
+    title: ownerConfigs.name,
+    description: ownerMetaData.description,
+    id: ownerMetaData.url,
+    link: ownerMetaData.url,
+    language: 'en',
+    image: `${ownerMetaData.url}${ownerMetaData.image.url}`,
+    favicon: `${ownerMetaData.url}/favicon.ico`,
+    copyright: `All rights reserved ${new Date().getFullYear()}, ${ownerConfigs.name}`,
+    updated: new Date(),
+    feedLinks: {
+      json: `${ownerMetaData.url}/feed.json`,
+      atom: `${ownerMetaData.url}/feed.atom`,
+      rss: `${ownerMetaData.url}/feed.xml`,
+    },
+    author: {
+      name: ownerConfigs.name,
+      email: ownerConfigs.githubLink,
+      link: ownerConfigs.githubLink,
+    },
+  })
+
+  allDocs.map((post) => {
+    return feed.addItem({
+      title: post.title,
+      id: `${ownerMetaData.url}${post.slug}`,
+      guid: `${ownerMetaData.url}${post.slug}`,
+      link: `${ownerMetaData.url}${post.slug}`,
+      date: new Date(post.published_date),
+      description: post.body.raw.slice(0, 300),
+      author: [{ name: post.author, link: ownerConfigs.githubLink, email: ownerConfigs.githubLink }],
+      content: post.body.raw,
+      image: post.image,
+    })
+  })
+
+  fs.writeFileSync('./public/feed.xml', feed.rss2())
+  fs.writeFileSync('./public/atom.xml', feed.atom1())
+  fs.writeFileSync('./public/feed.json', feed.json1())
+}
